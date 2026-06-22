56092f7df562b64713e5482754fb5bf1e7f11645
MD-Shahzaib-Tevso <shahzaib@tevsotech.com>
2026-06-22 20:16:25 +0500

    feat(config): add provideAnimationsAsync to application configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { MessageService } from 'primeng/api';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
     MessageService,
+    provideAnimationsAsync(),
   ],
 };
